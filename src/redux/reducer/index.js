1b35869034c4415b781083c0ebc5b789fe646f72
The Francisco Molina <fdm.molina@gmail.com>
2022-04-12 18:00:13 -0300

    action y reducer de getproduct y get name

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,16 +1,47 @@
 const initialState = {
-    user: null
-}
+  products: [],
+  backup: [],
+  pets: [],
+  users: [],
+  filter: [],
+  details: {},
+  user: null,
+};
 
 function rootReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.payload
-            }
-        default: return state;
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    case `GET_TOTAL_PRODUCT`:
+      return {
+        ...state,
+        products: action.payload,
+        backup: action.payload,
+      };
+
+    case `GET_BY_NAME`: {
+      const nameSearch = state.products.filter((e) => {
+        return e.title === action.payload;
+      });
+      if (nameSearch.length !== 0) {
+        return {
+          ...state,
+          backup: nameSearch,
+        };
+      } else {
+        return {
+          ...state,
+          products: false,
+        };
+      }
     }
+    default:
+      return state;
+  }
 }
 
-export default rootReducer
+export default rootReducer;
