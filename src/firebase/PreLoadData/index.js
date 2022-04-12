029baebb2a3455924e09909cd126a1348f67cb35
NachoEuclides <48378373+TuMandado@users.noreply.github.com>
2022-04-12 09:52:05 -0300

    Random data generation (#7)
    * pets folder
    
    * Create index.js

---
+++ src/firebase/PreLoadData/index.js
@@ -0,0 +1,47 @@
+// - Users:
+//     created: timestamp
+//     updated: timestamp
+//     mail: string
+//     role: admin, client.
+//     pets: array de uids
+//     carrito de compras: obj {
+//                 title
+//                 price
+//                 quantity
+//                     }
+//     compras: array de obj:[obj {
+//                 title
+//                 price
+//                 quantity
+//                 state
+//                 timestamp
+//                     }]
+//     direccion: obj:{
+//             string
+//             number
+//                 }
+// - Productos:
+//     created: timestamp
+//     updated: timestamp
+//     title: string
+//     price: number
+//     stock: number
+//     info: string
+//     animalCategory: [strings]
+//     category: [strings]
+
+// - Mascotas:
+//     created: timestamp
+//     updated: timestamp
+//     state: en adopcion  perdida  encontrado || nada
+//     owner: uid
+//     category: string
+//     sexo: string
+//     description: string
+//     ubicacion: obj {
+//             longitude: number
+//             latitude: number
+//                 }
+//     photos:[strings]
+
+
