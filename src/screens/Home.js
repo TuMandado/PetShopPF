0821d9d4a790bd51e82486408ba677d323d917cf
The Francisco Molina <fdm.molina@gmail.com>
2022-04-12 20:12:21 -0300

    Los volvi a como estaban.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,29 +1,15 @@
-
 import React from "react";
-import { firebaseApp } from "../firebase/credenciales";
-import { Timestamp } from "firebase/firestore";
-import { getAuth, signOut } from "firebase/auth";
-import { Navbar } from "../components/navbar/Navbar.jsx";
-const auth = getAuth(firebaseApp);
-import React from 'react'
 // import { firebaseApp } from "../firebase/credenciales";
 // import { Timestamp } from "firebase/firestore";
 // import { getAuth, signOut } from "firebase/auth";
 // const auth = getAuth(firebaseApp);
-import { signOutUsuario } from '../firebase/auth'
-
+import { signOutUsuario } from "../firebase/auth";
 
 function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home</h1>
-      <button onClick={() => signOut(auth)}>Cerrar cession</button>
-
-        <h1>Home</h1>
-        <button onClick={() => signOutUsuario()}>
-            Cerrar cession
-        </button>
+      <button onClick={() => signOutUsuario()}>Cerrar cession</button>
     </div>
   );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,48 +1,15 @@
 import React, { useState } from "react";
-import { firebaseApp } from "../firebase/credenciales";
-import { Timestamp } from "firebase/firestore";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { uploadUser } from "../firebase/Users";
-const auth = getAuth(firebaseApp);
 import { signInUsuario, registrarUsuario } from "../firebase/auth";
-
 
 function Login() {
   const [isRegistrando, setIsRegistrando] = useState(false);
-  const signInUsuario = (email, password) => {
-    auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((user) => {
-        console.log("Usuario iniciado sesion: ", user);
-      });
-  };
 
-  async function registrarUsuario(email, password, role) {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((user) => {
-        console.log("Usuario registrado: ", user);
-        uploadUser(user.user.uid, {
-          email: user.user.email,
-          role: role,
-          uid: user.user.uid,
-          createdAt: Timestamp.now(),
-          updatedAt: Timestamp.now(),
-        });
-      })
-      .catch((error) => {
-        console.log("Error al registrar usuario: ", error);
-      });
-  }
   const submitHandler = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
     const role = "Cliente";
-    console.log("Formulario enviado", email, password, role);
+
     if (isRegistrando) {
       registrarUsuario(email, password, role);
     } else {
