3c848bd4f9a0e10d55f7c47292bccf9c691c2188
Leonardo Fernandez <leonardojulianfernandez@gmail.com>
2022-04-13 12:08:28 -0300

    anuncios

--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: #92C064;
+  color: #067A4D;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+  padding-right: 20px;
+  width: cover;
+  ${mobile({ display: 'none'})}
+`;
 
 const Announcement = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <Container> Colabora con adopciones, mascotas perdidas o encontradas y obtene beneficios en nustra tienda  </Container>;
+};
 
-export default Announcement
+export default Announcement;
