35bf2266d15fa6297f7b222a74c34b8a46af5bd5
Leonardo Fernandez <leonardojulianfernandez@gmail.com>
2022-04-13 11:51:07 -0300

    slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,11 +1,168 @@
-import React from 'react'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import { useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+import { sliderItems } from '../data'
+import { mobile } from '../responsive'
+import { Link } from 'react-router-dom'
+
+
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0)
+  const vidRef = useRef()
+  const handleClick = direction => {
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 3)
+    } else {
+      setSlideIndex(slideIndex < 3 ? slideIndex + 1 : 0)
+    }
+  }
+
+  useEffect(() => {
+    vidRef.current.play()
+  }, [])
   return (
-    <div>
-      
-    </div>
+    <Div>
+      {/* <Title> ¿ ?</Title> */}
+      <Container>
+        <Arrow direction='left' onClick={() => handleClick('left')}>
+          <ArrowLeftOutlined />
+        </Arrow>
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map(item => (
+            <Slide bg={item.bg} key={item.id}>
+                  <ImgContainer>
+                    <Image src={item.img} />
+                  </ImgContainer>
+                  <InfoContainer>
+                    <Title>{item.title}</Title>
+                    <Desc>{item.desc}</Desc>
+                    <Link to='/pets'>
+                      <Button>mascotas</Button>
+                    </Link>
+                  </InfoContainer>
+            </Slide>
+          ))}
+        </Wrapper>
+        <Arrow direction='right' onClick={() => handleClick('right')}>
+          <ArrowRightOutlined />
+        </Arrow>
+      </Container>
+    </Div>
   )
 }
 
 export default Slider
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  ${mobile({ display: 'none' })}
+  @media (max-width: 320px) {
+    display: none;
+   
+  // }
+  // @media (min-width: 390px) {
+  //   display: none;
+    
+  // }
+`
+
+const Container = styled.div`
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+  ${mobile({
+    display: 'none'
+  })} // @media (min-width: 320px) {
+  //   display: none;
+  //   padding-top: 10px;
+  // }
+  // @media (min-width: 390px) {
+  //   display: none;
+  // }
+`
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #0ACF83;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${props => props.direction === 'left' && '10px'};
+  right: ${props => props.direction === 'right' && '10px'};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props => props.slideIndex * -100}vw);
+`
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  background-color: #${props => props.bg};
+`
+
+const ImgContainer = styled.div`
+  width: 100%;
+  flex: 2;
+  align-self: stretch;
+`
+
+const Image = styled.img`
+  height: 80%;
+  width: 100%;
+  ${mobile({ display: 'none' })}
+`
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`
+const Title = styled.h1`
+  color: #8aa290;
+  font-size: 40px;
+  margin: 6px;
+  ${mobile({ display: 'none' })}
+`
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background: #0ACF83;
+  cursor: pointer;
+  border-radius: 12px;
+  // position: absolute;
+  // width: 156px;
+  // height: 64px;
+  // left: 375px;
+  // top: 448px;
+`
+
+// <video  ref={vidRef} muted autoPlay loop src={videoLanding} />;
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const sliderItems = [
+
+  {
+    id: 0,
+    video:"",
+    bg: '8aa290',
+    title: '',
+    desc:
+      '',
+    info:
+      ''
+  },
+  {
+    id: 1,
+    img: 'https://www.webconsultas.com/sites/default/files/styles/wc_adaptive_noticia__small/public/noticias/web-adopcion-mascotas.jpg',
+    title: 'Adopta',
+    desc: 'Adopta una mascota, salva una vida y gana un amigo incondicional',
+    info: '',
+    bg: '#0ACF83'
+  },
+  {
+    id: 2,
+    img: 'https://forsythhumane.org/wp-content/uploads/2021/07/Lost-dog-1024x1024.png',
+    title: 'Encontraste una mascota perdida?',
+    desc: 'Si encontraste una mascota perdida podes publicarla y ayudar a devolverla a su hogar',
+    info: '',
+    bg: '#067A4D'
+  },
+  {
+    id: 3,
+    img:  'https://www.eluniversal.com.co/binrepository/1054x700/0c0/0d0/none/13704/GUPG/14386515-l_5023847_20210818180739.jpg',
+    title: 'Perdiste a tu mascota?',
+    desc: 'Publica tu mascota estraviada para que si alguien la encuentra se comunique con vos',
+    info:'',
+    bg: '#92C064'
+  }
+
+]
