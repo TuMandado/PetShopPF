cead662b105c9228fdb279dab96a0b997268f3f4
nicolasmoises <86451624+nicolasmoises@users.noreply.github.com>
2022-04-13 00:11:34 -0300

    componentes-products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
+import { useDispatch , useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { getTotalProducts } from '../../redux/actions'
+import Product from '../product/Product'
 
-const Products = ({imagen, title, info, price, animalCategory}) => {
+const Products = () => {
+  const dispatch = useDispatch()
+  const allProducts = useSelector(state => state.backup)
+  console.log(allProducts)
+  const [loader, setLoader] = useState(true)
+  const [error, setError] = useState(false )
+
+  useEffect(() => {
+    dispatch(getTotalProducts())
+    .then((Response) => {
+      setLoader(false)
+
+    })
+    .catch(error => setError (error.message))
+  }, [dispatch])
+
+  if(loader){
+    return (
+      <div>
+        <h1>Cargando..</h1>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <div>
-        <img src={imagen} alt="imagen not found" />
-      </div>
-      <div>
-        <h3>{title}</h3>
-      </div>
-      <div>
-        <p>{info}</p>
-      </div>
-      <div>
-        <p>{price}</p>
-      </div>
-      <div>
-        <h3> categoria de aniaml : {animalCategory?.map((t,i) => <div key={i}> {t} </div> )}</h3>
-      </div>
+      {
+        allProducts.length > 0 ? (
+          allProducts.map(e => {
+            return (
+              <div>
+                <Product title={e.data.title} imagen={e.data.imagen} info= {e.data.info} price={e.data.price} animalCategory={e.data.animalCategory} category= {e.data.category}/>
+              </div>
+            )
+          })
+        ): (
+        <div>
+          <h1>Error, no hay datos</h1>
+        </div>
+      )
+      } 
+
     </div>
   )
 }
