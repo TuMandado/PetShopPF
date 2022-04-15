08e38ba412eb29c7f05aa0f1537072771ebbadf0
Leonardo Fernandez <leonardojulianfernandez@gmail.com>
2022-04-14 23:16:05 -0300

    categorias

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -11,9 +11,9 @@ const Slider = () => {
   const vidRef = useRef()
   const handleClick = direction => {
     if (direction === 'left') {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 3)
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
     } else {
-      setSlideIndex(slideIndex < 3 ? slideIndex + 1 : 0)
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
     }
   }
 
