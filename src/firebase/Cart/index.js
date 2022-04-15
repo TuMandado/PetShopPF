6ff11b98825e943997c3c40a4fc69d918f946178
Eugenioqur <eugequr@gmail.com>
2022-04-14 22:29:13 -0300

    SumarItems

--- src/firebase/Cart/index.js
+++ src/firebase/Cart/index.js
@@ -1,10 +1,31 @@
 import {firebase, db} from '../credenciales'
-import { doc, setDoc, Timestamp, deleteDoc, getDoc, getDocs, updateDoc ,collection } from "firebase/firestore";
+import { doc, setDoc, Timestamp, deleteDoc, getDoc, getDocs, updateDoc ,collection, serverTimestamp} from "firebase/firestore";
 import { async } from '@firebase/util';
 
 var collectionRef = "Cart";
 
-export async function uploadCart(data, uid) {
+export const createId = async () => {
+    var id = "";
+    var exists = true;
+    while (exists) {
+        id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        exists = await checkIfExists(id);
+    }
+    return id;
+}
+
+const checkIfExists = async (id) => {
+    var exists = false;
+    await getDoc(collectionRef, id).then(doc => {
+        if (doc.exists) {
+            exists = true;
+        }
+    });
+    return exists;
+}
+
+export async function uploadCart(data) {
+    let uid = createId()
     await setDoc(doc(db, collectionRef, uid), data);
   }
 
@@ -36,4 +57,41 @@ export async function getAllCarts() {
 
 export async function editCart(uid,data){
     await updateDoc(doc(db, collectionRef, uid), data);
+  }
+
+function cartLocalStorage(data){
+    localStorage.setItem("cart",JSON.stringify(data))
+}
+
+export async function cartOpen(userUid){
+    let cars = await getAllCarts()
+    let open = cars.filter(el => el.data.close === false) 
+    if (open.length){
+        let cartUser= open.filter(el => el.uid === userUid)
+        if(cartUser.length){
+            return cartUser
+        }
+    }
+}
+
+// export async function newCart(user,data,uid){
+//     if (user){
+//         prevCart = cartOpen(user.uid)
+//         if (prevCart){
+//             if(localStorage.getItem('cart')){
+                
+//             }
+//             editCart()
+//         }else{
+//             uploadCart()
+//         }
+//     }
+// }
+
+export function sumarItems(db,localS){
+    let finishdb = db
+    let keys = Object.keys(localS)
+    keys.forEach((el)=> finishdb[el] ? finishdb[el].cantidad = finishdb[el].cantidad + localS[el].cantidad : finishdb[el]=localS[el])
+    
+    return finishdb
   }
