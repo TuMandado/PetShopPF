15efd9752d8d32dd19661e2e337bbd5fc00e9ae0
Eugenioqur <eugequr@gmail.com>
2022-04-17 13:14:51 -0300

    error1

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,6 +1,7 @@
-import { addCartItem } from "../../firebase/Cart";
+import { addCartItem, cartOpen, cartOpenUs } from "../../firebase/Cart";
 
 export const ADD_ITEM = 'ADD_ITEM';
+export const OPEN_CART = 'OPEN_CART';
 
 export function addItemCartFront(payload) {
     return async function (dispatch) {
@@ -10,6 +11,22 @@ export function addItemCartFront(payload) {
             return dispatch({
                 type: ADD_ITEM,
                 payload: jsonProduct,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+}
+
+export function openCartFront(payload) {
+    return async function (dispatch) {
+        try {
+            console.log("payload", payload)
+            let jsonProduct = await cartOpenUs(payload);
+            console.log("OpenCart Flag-", jsonProduct);
+            return dispatch({
+             type: OPEN_CART,
+             payload: jsonProduct,
             });
         } catch (error) {
             console.log(error);
