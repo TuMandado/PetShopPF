75ee3159e7497edc56be9f70033ed97259303693
nicolasmoises <86451624+nicolasmoises@users.noreply.github.com>
2022-04-23 16:53:15 -0300

    petCreated con validaciones falta css

--- src/components/petCreated/petCreated.jsx
+++ src/components/petCreated/petCreated.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {Link,useNavigate} from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getTotalPets, getTotalCategoryPets, postPets} from '../../redux/actions/index';
+import { getTotalPets, getTotalCategoryPets, postPets, getStatePet} from '../../redux/actions/index';
 
 function validadora (input) {
     let error = {}
-    if(!input.name || input.name.length <3) {
+    if((!input.name || input.name.length <3) || input.name.search(/^[^$%&|<>#]*$/)) {
         error.name = 'ingrese un nombre por favor'
     } else if (!input.owner) {
         error.owner = 'ingrese el nombre del dueño'
-    } else if ((input.sexo === 'masculino') || (input.sexo === 'femenino')) {
-        error.sexo = 'se ingreso el sexo correcto?'
+    } else if (!input.sexo) {
+        error.sexo = 'ingrese sexo'
     } else if (input.category.length === 0) {
         error.category = 'ingrese una categoria porfitas'
     } else if (!input.photos) {
@@ -30,7 +30,10 @@ const PetCreated = ()=> {
     let pets = useSelector((state) => state.clientReducer.pets)
     console.log('esto es pets', pets)
     const petsCategory = useSelector((state) => state.clientReducer.categoryPets)
-    console.log('esto es petsCategory', petsCategory)
+    // console.log('esto es petsCategory', petsCategory)
+    const statePet = useSelector((state) => state.clientReducer.statePets)
+    console.log('esto es statePet', statePet)
+
 
     const [errors, setErrors] = useState({})
     const [input, setInput] = useState({
@@ -62,35 +65,47 @@ const PetCreated = ()=> {
     function handleSelect(e) {
         setInput({
             ...input,
-            category: input.category.includes(e.target.value)? 
-            input.category :
-            [...input.category, e.target.value]
+            category: e.target.value
         })
     }
-
-    function handleDelete (e) {
+    function handleSelect2 (e) {
         setInput({
             ...input,
-            category: input.category.filter(category => category !== e)
+            state: e.target.value
         })
-
     }
+// input.category.includes(e.target.value)? 
+//             input.category :
+//             [...input.category, e.target.value]
+    // function handleDelete (e) {
+    //     setInput({
+    //         ...input,
+    //         category: input.category.filter(category => category !== e)
+    //     })
+
+    //  }
 
     function handleSubmit(e) {
         e.preventDefault()
         if(input.name.trim() === '' ||  input.name.search(/^[^$%&|<>#]*$/)) {
-            return alert('Ingrese nombre adecuado')
+            return alert('Ingrese un nomre o Ingrese nombre adecuado')
     }  else if (
         pets.find(e => e.data.name.toLowerCase().trim() === input.name.toLowerCase().trim())
     ) {
-        return alert(`El Producto ${input.name} ya existe`)
-    } else if (input.owner.trim() === ''||  input.name.search(/^[^$%&|<>#]*$/) ) {
-        return alert ('por favor ingrese dueño')
-    } else if (input.sexo.trim() === ''||  input.name.search(/^[^$%&|<>#]*$/) ) {
-        return alert ('por favor ingrese sexo adecuado')
+        return alert(`La mascota ${input.name} ya existe`)
+    } else if (input.owner.trim() === ''||  input.owner.search(/^[^$%&|<>#]*$/) ) {
+        return alert ('Ingrese un nomre o Ingrese nombre adecuado')
+    } else if ((input.sexo.trim() === ''||  input.sexo.search(/^[^$%&|<>#]*$/)) ) {
+        return alert ('por favor ingrese sexo o el sexo adecuado')
     } else if (input.category.trim() === '') {
         return alert('selecciona una categoria de animal por favor')
-    } 
+    }  else if (input.state.trim() === '') {
+        return alert('selecciona el estado en el que se encuentra su mascota')
+    } else if (input.photos.trim() === ''||  input.photos.search(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|svg)/gi)) {
+        return alert( 'por favor ingrese una imagen de su mascota')
+    } else if(input.description.trim() === '' ||  input.description.search(/^[^$%&|<>#]*$/)) {
+        return alert('por favor ingrese una descripcion adecuada')
+    }
     
     
     
@@ -117,6 +132,7 @@ const PetCreated = ()=> {
     useEffect(() => {
         dispatch(getTotalPets());
         dispatch(getTotalCategoryPets())
+        dispatch(getStatePet())
 
     },[dispatch])
 
@@ -143,6 +159,7 @@ const PetCreated = ()=> {
                    }
 
                    </div>
+                   <br />
                    <div>
                        <label>Dueño: </label>
                        <br />
@@ -159,6 +176,7 @@ const PetCreated = ()=> {
                        )
                    }
                    </div>
+                   <br />
                    <div>
                        <label >Sexo: </label>
                        <br />
@@ -175,6 +193,7 @@ const PetCreated = ()=> {
                        )
                    }
                    </div>
+                   <br />
                    <div>
                        <select onChange={e => handleSelect(e)}>
                            <option disabled> Seleccion de categoria</option>
@@ -202,6 +221,18 @@ const PetCreated = ()=> {
                            )
                        }
                    </div>
+                   <br />
+                   <div>
+                       <select onChange={e => handleSelect2(e)}>
+                           <option  disabled>Como se encuentra su mascota</option>
+                           {
+                               statePet?.map(e => (
+                                   <option value={e}> {e}</option>
+                               ))
+                           }
+                       </select>
+                   </div>
+                   <br />
                    <div>
                        <label >Agrege una imagen de su mascota</label>
                        <br />
@@ -218,6 +249,7 @@ const PetCreated = ()=> {
                        }
 
                    </div>
+                   <br />
                    <div>
                    <label >Agrege una Descripcion</label>
                        <br />
@@ -234,11 +266,6 @@ const PetCreated = ()=> {
                             )
 
                            }
-                   </div>
-                   <div>
-                       <select >
-                           <option disabled > Seleccione su estado</option>
-                       </select>
                    </div>
                    <button type='submit'>Crear Mascota</button>
                </form> 
