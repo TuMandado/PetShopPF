05be4e8f16c1834b4e3898bfed201fc88902261b
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-04-23 20:18:31 -0300

    test v2

--- src/components/petDetails/petDetails.jsx
+++ src/components/petDetails/petDetails.jsx
@@ -8,77 +8,84 @@ import Footer from "../../components/footer/Footer";
 import { petDetails, detailVacio } from "../../redux/actions";
 import styled from "styled-components";
 
-const Container = styled.div``;
+
+const Hola = styled.div`
+`
+const Chau = styled.span`
+`
 
 const PetDetail = () => {
-  // const user = useSelector((state) => state.clientReducer.user);
-  const pet = useSelector((state) => state.clientReducer.backupDetail);
-  const dispatch = useDispatch();
-  const uid = useParams();
-  console.log("Uid flag =>", uid);
-  console.log("PETS =>", pet);
+    // const user = useSelector((state) => state.clientReducer.user);
+    const pet = useSelector((state) => state.clientReducer.backupDetail);
+    const dispatch = useDispatch();
+    const uid = useParams();
+    console.log("Uid flag =>", uid);
 
-  useEffect(() => {
-    dispatch(petDetails(uid.id));
-    return function () {
-      dispatch(detailVacio());
-    };
-  }, [dispatch, uid.id]);
 
-  if (!pet.name) {
+
+    console.log("PETS =>", pet);
+
+    useEffect(() => {
+        dispatch(petDetails(uid.id));
+        return function () {
+            dispatch(detailVacio());
+        };
+    },)
+
+    if (!pet.name) {
+        return (
+            <div>
+                <div>
+                    <Navbar />
+                </div>
+                <div>
+                    <Loader />
+                </div>
+            </div>
+        );
+    }
+
     return (
-      <div>
         <div>
-          <Navbar />
+            <Navbar />
+            <div>
+                <div>
+                    <div>
+                        <h1>{pet.name}</h1>
+                    </div>
+                    <div>
+                        <div>
+                            <h5>Animal: {pet.category}</h5>
+                        </div>
+                        <div>
+                            <h5>Estado: {pet.state}</h5>
+                        </div>
+                        <div>
+                            <h5>Sexo: {pet.sexo}</h5>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p>{pet.description}</p>
+                        </div>
+                        <div>
+                            <button>Algo</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src={pet.photos || "https://imgur.com/lhLYKao"} alt="imagen" />
+                </div>
+                <div>
+                    <Link to="/pets">
+                        <button>Volver</button>
+                    </Link>
+                </div>
+            </div>
+
+            {/* <Footer /> */}
         </div>
-        <div>
-          <Loader />
-        </div>
-      </div>
     );
-  }
-
-  return (
-    <div>
-      <Navbar />
-      <div>
-        <div>
-          <div>
-            <h1>{pet.name}</h1>
-          </div>
-          <div>
-            <div>
-              <h5>Animal: {pet.category}</h5>
-            </div>
-            <div>
-              <h5>Estado: {pet.state}</h5>
-            </div>
-            <div>
-              <h5>Sexo: {pet.sexo}</h5>
-            </div>
-          </div>
-          <div>
-            <div>
-              <p>{pet.description}</p>
-            </div>
-            <div>
-              <button>Algo</button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <img src={pet.photos || "https://imgur.com/lhLYKao"} alt="imagen" />
-        </div>
-        <div>
-          <Link to="/pets">
-            <button>Volver</button>
-          </Link>
-        </div>
-      </div>
-
-      {/* <Footer /> */}
-    </div>
-  );
 };
 
 export default PetDetail;
