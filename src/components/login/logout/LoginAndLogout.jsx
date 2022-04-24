851ab61770f2245ea1365aee0b3139d611bb9c8f
The Francisco Molina <fdm.molina@gmail.com>
2022-04-24 07:54:00 -0300

    Revert "quite iconos a rutas que ya estan en navbar. Agrega a /usersettings"
    This reverts commit 50760b5b31ee7e0d745b01cc37fc2f06dd3ab7e0.

--- src/components/login/logout/LoginAndLogout.jsx
+++ src/components/login/logout/LoginAndLogout.jsx
@@ -1,43 +1,63 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import icoPanel from "../../../assets/settings.png";
+import icoMarket from "../../../assets/market.png";
+import IcoProducts from "../../../assets/tienda_menu.png";
+import IcoPets from "../../../assets/patita_menu.png";
 import icoLogOut from "../../../assets/cerrar.png";
+import icoUser from "../../../assets/user.png";
 import { SupervisorAccountRounded } from "@material-ui/icons";
 import { signOutUsuario } from "../../../firebase/auth";
 import { useSelector } from "react-redux";
+
 
 export const LoginLogout = () => {
   var user = useSelector((state) => state.clientReducer.user);
   return (
     <ModalLogin>
       <Option>
-        {user && user.role.toLowerCase() === "admin" && (
+      {user && user.role.toLowerCase() === "admin" && (
           <Link to={"/admin"}>
             <BtnOption>
               <SupervisorAccountRounded className="sidebarIcon" />
               <Text>Opciones de administrador</Text>
             </BtnOption>
           </Link>
-        )}
+      )}
       </Option>
       <Option>
-        <Link to={"/usersettings"}>
+        <Link to={"/products"}>
           <BtnOption>
-            <BtnIcon src={icoPanel} alt="user settings" />
-            <Text>Ver Cuenta</Text>
+            <BtnIcon src={IcoProducts} alt="products" />
+            <Text>Ver Tienda</Text>
           </BtnOption>
         </Link>
       </Option>
       <Option>
-        {user && (
+        <Link to={"/pets"}>
+          <BtnOption>
+            <BtnIcon src={IcoPets} alt="mascotas" />
+            <Text>Ver Mascotas</Text>
+          </BtnOption>
+        </Link>
+      </Option>
+      <Option>
+        <Link to={"/cart"}>
+          <BtnOption>
+            <BtnIcon src={icoMarket} alt="market" />
+            <Text>Ir al carrito</Text>
+          </BtnOption>
+        </Link>
+      </Option>
+      <Option>
+        {user &&
           <BtnOption onClick={() => signOutUsuario()}>
             <Link to={"/"}>
               <BtnIcon src={icoLogOut} alt="logout" />{" "}
               <Text>Cerrar Sesión</Text>
             </Link>
           </BtnOption>
-        )}
+         }
       </Option>
     </ModalLogin>
   );
