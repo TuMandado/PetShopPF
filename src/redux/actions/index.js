eefa1f05d56f2e412d9dd37a4c66c4c7b7678dec
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-05-04 00:42:07 -0300

    Fix ClientReducer y Action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,22 +1,22 @@
 import {
-  getAllProducts,
-  getProduct,
-  getAllProductsCategories,
-  getAllProductsSubCategory,
-  getAllProductsAnimal,
-  filterProducts,
-  // uploadProduct,
-  editProduct,
+    getAllProducts,
+    getProduct,
+    getAllProductsCategories,
+    getAllProductsSubCategory,
+    getAllProductsAnimal,
+    filterProducts,
+    // uploadProduct,
+    editProduct,
 } from "../../firebase/Products/index";
 import {
-  getAllPets,
-  filterPets,
-  getAllCategories,
-  uploadPet,
-  getPet,
-  getStatePets,
-  searchPet,
-  deletePet,
+    getAllPets,
+    filterPets,
+    getAllCategories,
+    uploadPet,
+    getPet,
+    getStatePets,
+    searchPet,
+    deletePet,
 } from "../../firebase/Pets/index";
 // import { async } from "@firebase/util";
 import { loginCart, getOrderByUser } from "../../firebase/Cart";
@@ -29,278 +29,278 @@ import Swal from "sweetalert2";
 // import { getAllPets } from "../../firebase/Pets/index";
 
 export function setUser(payload) {
-  return async function (dispatch) {
-    try {
-      let jsonProduct = await loginCart(payload);
-      console.log("-login Flag-", jsonProduct);
-      return dispatch({
-        type: "SET_USER",
-        payload,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  //     return {
-  //         type: "SET_USER",
-  //         payload,
-  //     };
+    return async function (dispatch) {
+        try {
+            let jsonProduct = await loginCart(payload);
+            console.log("-login Flag-", jsonProduct);
+            return dispatch({
+                type: "SET_USER",
+                payload,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    //     return {
+    //         type: "SET_USER",
+    //         payload,
+    //     };
 }
 
 export function getTotalProducts() {
-  return async function (dispatch) {
-    try {
-      let jsonProduct = await getAllProducts();
-      console.log("-Action Flag-", jsonProduct);
-      return dispatch({
-        type: `GET_TOTAL_PRODUCT`,
-        payload: jsonProduct,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            let jsonProduct = await getAllProducts();
+            console.log("-Action Flag-", jsonProduct);
+            return dispatch({
+                type: `GET_TOTAL_PRODUCT`,
+                payload: jsonProduct,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 export function getProductName(name) {
-  return async function (dispatch) {
-    try {
-      if (name.search(/^[a-zA-Zñáéíóúü]*$/)) {
-        return Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "El nombre solo debe contener letras. ¡Intenta de nuevo!",
-        });
-      }
-      let jsonProduct = await getAllProducts(name);
+    return async function (dispatch) {
+        try {
+            if (name.search(/^[a-zA-Zñáéíóúü]*$/)) {
+                return Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "El nombre solo debe contener letras. ¡Intenta de nuevo!",
+                });
+            }
+            let jsonProduct = await getAllProducts(name);
 
-      return dispatch({
-        type: `GET_BY_NAME`,
-        payload: jsonProduct,
-      });
-    } catch (error) {
-      return Swal.fire({
-        icon: "error",
-        title: "Error...",
-        text: "No encontramos un producto con ese nombre.",
-      });
-    }
-  };
+            return dispatch({
+                type: `GET_BY_NAME`,
+                payload: jsonProduct,
+            });
+        } catch (error) {
+            return Swal.fire({
+                icon: "error",
+                title: "Error...",
+                text: "No encontramos un producto con ese nombre.",
+            });
+        }
+    };
 }
 
 export function getTotalPets() {
-  return async function (dispatch) {
-    try {
-      let jsonPets = await getAllPets();
-      // console.log("esto es jsonPets", jsonPets);
-      return dispatch({
-        type: "GET_ALL_PETS",
-        payload: jsonPets,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            let jsonPets = await getAllPets();
+            // console.log("esto es jsonPets", jsonPets);
+            return dispatch({
+                type: "GET_ALL_PETS",
+                payload: jsonPets,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 export function setLoading(value) {
-  return {
-    type: `SET_LOADING`,
-    payload: value,
-  };
+    return {
+        type: `SET_LOADING`,
+        payload: value,
+    };
 }
 
 export function getDetailProducts(uid) {
-  return async function (dispatch) {
-    try {
-      let jsonDetail = await getProduct(uid);
-      console.log("jsonDetail", jsonDetail);
-      return dispatch({
-        type: "GET_DETAIL_PRODUCTS",
-        payload: jsonDetail,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            let jsonDetail = await getProduct(uid);
+            console.log("jsonDetail", jsonDetail);
+            return dispatch({
+                type: "GET_DETAIL_PRODUCTS",
+                payload: jsonDetail,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 export function detailVacio() {
-  return {
-    type: "DETAIL_VACIO",
-  };
+    return {
+        type: "DETAIL_VACIO",
+    };
 }
 
 export function getProductsCategories() {
-  return async function (dispatch) {
-    try {
-      const categories = await getAllProductsCategories();
-      const subcategories = await getAllProductsSubCategory();
+    return async function (dispatch) {
+        try {
+            const categories = await getAllProductsCategories();
+            const subcategories = await getAllProductsSubCategory();
 
-      return dispatch({
-        type: "GET_PRODUCTS_CATEGORIES",
-        payload: { categories, subcategories },
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+            return dispatch({
+                type: "GET_PRODUCTS_CATEGORIES",
+                payload: { categories, subcategories },
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
 }
 
 export function getAnimalCategories() {
-  return async function (dispatch) {
-    try {
-      const categories = await getAllProductsAnimal();
-      return dispatch({
-        type: "GET_ANIMAL_CATEGORIES",
-        payload: categories,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            const categories = await getAllProductsAnimal();
+            return dispatch({
+                type: "GET_ANIMAL_CATEGORIES",
+                payload: categories,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
 }
 
 export function deleteFilters() {
-  return {
-    type: "DELETE_FILTERS",
-  };
+    return {
+        type: "DELETE_FILTERS",
+    };
 }
 
 export function filterAllProducts(array, category, animal, minPrice, maxPrice) {
-  return async function (dispatch) {
-    const response = await filterProducts(
-      array,
-      category,
-      animal,
-      minPrice,
-      maxPrice
-    );
-    console.log(response);
-    return dispatch({
-      type: "FILTER_PRODUCTS",
-      payload: response,
-    });
-  };
+    return async function (dispatch) {
+        const response = await filterProducts(
+            array,
+            category,
+            animal,
+            minPrice,
+            maxPrice
+        );
+        console.log(response);
+        return dispatch({
+            type: "FILTER_PRODUCTS",
+            payload: response,
+        });
+    };
 }
 
 export function getTotalCategoryPets() {
-  return async function (dispatch) {
-    const jsonCategoryPets = await getAllAnimalCategory();
-    console.log("esto es jsonCategoryPets", jsonCategoryPets);
-    return dispatch({
-      type: "GET_CATEGORY_PETS",
-      payload: jsonCategoryPets,
-    });
-  };
+    return async function (dispatch) {
+        const jsonCategoryPets = await getAllAnimalCategory();
+        console.log("esto es jsonCategoryPets", jsonCategoryPets);
+        return dispatch({
+            type: "GET_CATEGORY_PETS",
+            payload: jsonCategoryPets,
+        });
+    };
 }
 
 export function postPets(payload) {
-  return async function (dispatch) {
-    const jsonPetsPost = await uploadPet(payload);
-    console.log("esto es jsonPetsPost", jsonPetsPost);
-    return jsonPetsPost;
-  };
+    return async function (dispatch) {
+        const jsonPetsPost = await uploadPet(payload);
+        console.log("esto es jsonPetsPost", jsonPetsPost);
+        return jsonPetsPost;
+    };
 }
 
 export function petDetails(uid) {
-  return async function (dispatch) {
-    try {
-      let petDetail = await getPet(uid);
-      console.log("Pet Details =>", petDetail);
-      return dispatch({
-        type: "GET_DETAIL_PET",
-        payload: petDetail,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            let petDetail = await getPet(uid);
+            console.log("Pet Details =>", petDetail);
+            return dispatch({
+                type: "GET_DETAIL_PET",
+                payload: petDetail,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 export function getSpeciesPets() {
-  return async function (dispatch) {
-    const species = await getAllCategories();
-    dispatch({
-      type: "GET_SPECIES_PETS",
-      payload: species,
-    });
-  };
+    return async function (dispatch) {
+        const species = await getAllCategories();
+        dispatch({
+            type: "GET_SPECIES_PETS",
+            payload: species,
+        });
+    };
 }
 
 export function filterAllPets(array, animal, gender, state, owner) {
-  return async function (dispatch) {
-    const filteredPets = await filterPets(array, animal, gender, state, owner);
-    dispatch({
-      type: "FILTER_ALL_PETS",
-      payload: filteredPets,
-    });
-  };
+    return async function (dispatch) {
+        const filteredPets = await filterPets(array, animal, gender, state, owner);
+        dispatch({
+            type: "FILTER_ALL_PETS",
+            payload: filteredPets,
+        });
+    };
 }
 
 export function resetPetFilters() {
-  return {
-    type: "RESET_PET_FILTERS",
-  };
+    return {
+        type: "RESET_PET_FILTERS",
+    };
 }
 
 export function getStatePet() {
-  return async function (dispatch) {
-    const jsonState = await getStatePets();
-    dispatch({
-      type: "GET_STATE_PETS",
-      payload: jsonState,
-    });
-  };
+    return async function (dispatch) {
+        const jsonState = await getStatePets();
+        dispatch({
+            type: "GET_STATE_PETS",
+            payload: jsonState,
+        });
+    };
 }
 
 export function setSettings(payload) {
-  return {
-    type: "SET_SETTINGS",
-    payload,
-  };
+    return {
+        type: "SET_SETTINGS",
+        payload,
+    };
 }
 
 export function setVisitId(payload) {
-  return {
-    type: "SET_VISIT_ID",
-    payload,
-  };
+    return {
+        type: "SET_VISIT_ID",
+        payload,
+    };
 }
 
 //payload es un objeto con uid del producto y un objeto {stock: cantidad final del stock}
 export async function editStock(payload) {
-  return async function (dispatch) {
-    try {
-      let jsonStock = await editProduct(payload);
-      console.log("-stock Flag-", jsonStock);
-      return dispatch({
-        type: `EDIT_STOCK`,
-        payload: jsonStock,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            let jsonStock = await editProduct(payload);
+            console.log("-stock Flag-", jsonStock);
+            return dispatch({
+                type: `EDIT_STOCK`,
+                payload: jsonStock,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 export function filterPetByName(pets, name) {
-  return async function (dispatch) {
-    try {
-      const searchedPets = await searchPet(pets, name);
-      return dispatch({
-        type: "SEARCH_PET",
-        payload: searchedPets,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            const searchedPets = await searchPet(pets, name);
+            return dispatch({
+                type: "SEARCH_PET",
+                payload: searchedPets,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 
 export function getMyOrders(uid) {
-    return async function(dispatch) {
+    return async function (dispatch) {
         try {
             const allOrders = await getOrderByUser(uid)
             dispatch({
@@ -308,23 +308,23 @@ export function getMyOrders(uid) {
                 payload: allOrders
             })
         }
-        catch(error) {
+        catch (error) {
             console.log(error)
         }
     }
-
+}
 export function deleteThisPet(uid) {
-  return async function (dispatch) {
-    try {
-      let jsonDelete = await deletePet(uid);
-      console.log('esto es jsonDele', jsonDelete, 'esto es uid', uid)
-      return dispatch({
-        type: `DELETE_PET`,
-        payload: uid,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    return async function (dispatch) {
+        try {
+            let jsonDelete = await deletePet(uid);
+            console.log('esto es jsonDele', jsonDelete, 'esto es uid', uid)
+            return dispatch({
+                type: `DELETE_PET`,
+                payload: uid,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
 }
