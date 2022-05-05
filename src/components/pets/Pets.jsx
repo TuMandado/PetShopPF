4c03802a7a8086bd78c32559a079296766a960c1
The Francisco Molina <fdm.molina@gmail.com>
2022-05-05 03:47:31 -0300

    muestra ciudad pet nuevos + stylos admin

--- src/components/pets/Pets.jsx
+++ src/components/pets/Pets.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
-import { getTotalPets, getSpeciesPets, getStateAllPets } from "../../redux/actions";
+import {
+  getTotalPets,
+  getSpeciesPets,
+  getStateAllPets,
+} from "../../redux/actions";
 import Pet from "../pet/Pet";
 import { Loader } from "../../page/loader/Loader";
 import { Link } from "react-router-dom";
@@ -9,8 +13,6 @@ import { useNavigate } from "react-router-dom";
 import Footer from "../../components/footer/Footer";
 import { AsidePets } from "../../components/aside/Aside Pets";
 import styled from "styled-components";
-
-
 
 const MainAllCards = styled.div`
   box-sizing: border-box;
@@ -25,88 +27,94 @@ const MainAllCards = styled.div`
 `;
 
 const Error = styled.h1`
-    width: 30%;
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 1.5em;
-    line-height: 22px;
-    position: absolute;
-    right: 32%;
-    top: 16%;
-`
+  width: 30%;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.5em;
+  line-height: 22px;
+  position: absolute;
+  right: 32%;
+  top: 16%;
+`;
 
 const CatSleeping = styled.img`
-    position: absolute;
-    top: 15%;
-    left: 40%;
-`
-
+  position: absolute;
+  top: 15%;
+  left: 40%;
+`;
 
 const Pets = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const allPets = useSelector((state) => state.clientReducer.pets);
-    // console.log('pets', allPets)
-    const [loader, setLoader] = useState(true);
-    // eslint-disable-next-line no-unused-vars
-    const [error, setError] = useState(false);
+  const allPets = useSelector((state) => state.clientReducer.pets);
+  // console.log('pets', allPets)
+  const [loader, setLoader] = useState(true);
+  // eslint-disable-next-line no-unused-vars
+  const [error, setError] = useState(false);
 
-    const navigatePet = (e) => {
-        navigate(`/pets/${e.currentTarget.id}`);
-    };
-    useEffect(() => {
-        console.log('esto es all pets', allPets)
-    }, [allPets])
+  const navigatePet = (e) => {
+    navigate(`/pets/${e.currentTarget.id}`);
+  };
+  useEffect(() => {
+    console.log("esto es all pets", allPets);
+  }, [allPets]);
 
-    useEffect(() => {
-        dispatch(getSpeciesPets())
-        dispatch(getTotalPets())
-            .then((response) => {
-                setLoader(false);
-            })
-            .catch((error) => setError(error.message));
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getSpeciesPets());
+    dispatch(getTotalPets())
+      .then((response) => {
+        setLoader(false);
+      })
+      .catch((error) => setError(error.message));
+  }, [dispatch]);
 
-    if (loader) {
-        return (
-            <div>
-                <Loader />
-            </div>
-        );
-    }
-
+  if (loader) {
     return (
-        <div>
-            <AsidePets />
-            <MainAllCards>
-                {allPets.length && typeof allPets !== 'string' ? (
-                    allPets.map((e) => {
-                        return (
-                            <div key={e.uid} id={e.uid} onClick={(e) => navigatePet(e)}>
-                                <Pet
-                                    state={e.data.state}
-                                    category={e.data.category}
-                                    sexo={e.data.sexo}
-                                    name={e.data.name}
-                                    description={e.data.description}
-                                    photos={e.data.photos}
-                                />
-                            </div>
-                        );
-                    })
-                ) : (
-                    <div>
-                        <Error>Vaya! Parece que no hemos encontrado ninguna mascota con dichas caracteristicas</Error>
-                        <CatSleeping src='https://31.media.tumblr.com/e9c5a6eb1241c1cd3f89e12e89874c66/tumblr_mv1vm39xs51rz5dlbo1_500.gif' alt='' />
-                    </div>
-                )}
-                <div></div>
-            </MainAllCards>
-            <Footer />
-        </div>
+      <div>
+        <Loader />
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <AsidePets />
+      <MainAllCards>
+        {allPets.length && typeof allPets !== "string" ? (
+          allPets.map((e) => {
+            return (
+              <div key={e.uid} id={e.uid} onClick={(e) => navigatePet(e)}>
+                <Pet
+                  state={e.data.state}
+                  category={e.data.category}
+                  sexo={e.data.sexo}
+                  name={e.data.name}
+                  description={e.data.description}
+                  photos={e.data.photos}
+                  city={e.data.city}
+                />
+              </div>
+            );
+          })
+        ) : (
+          <div>
+            <Error>
+              Vaya! Parece que no hemos encontrado ninguna mascota con dichas
+              caracteristicas
+            </Error>
+            <CatSleeping
+              src="https://31.media.tumblr.com/e9c5a6eb1241c1cd3f89e12e89874c66/tumblr_mv1vm39xs51rz5dlbo1_500.gif"
+              alt=""
+            />
+          </div>
+        )}
+        <div></div>
+      </MainAllCards>
+      <Footer />
+    </div>
+  );
 };
 
 export default Pets;
